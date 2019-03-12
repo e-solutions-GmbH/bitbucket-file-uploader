03417063ce9d9b72f7ff8f12f5d29c6d0610426d
fesc7420 <ferdinand.schneider@esolutions.de>
2019-03-12 17:08:58 +0100

    initial commit with getting started from dev portal

---
+++ src/popup.ts
@@ -0,0 +1,15 @@
+let changeColor: HTMLElement = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function (data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function (element) {
+    const color = (element.target as HTMLInputElement).value;
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+};
