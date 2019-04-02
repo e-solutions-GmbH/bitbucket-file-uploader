9fd81d69e37b3e1899396b80d3ddac4376f04d1e
fesc7420 <ferdinand.schneider@esolutions.de>
2019-04-02 16:56:14 +0200

    correct configuration of url matchings

--- src/popup.ts
+++ src/popup.ts
@@ -2,6 +2,7 @@ const upload: HTMLElement = document.getElementById('upload');
 
 upload.onclick = function (element) {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        // new URL(tabs[0].url).pathname.split('/');
         chrome.tabs.sendMessage(
             tabs[0].id, {type: 'uploadRequest'}, xhrResp => {
                 console.log('received response message');
