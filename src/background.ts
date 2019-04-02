62fac07342f69663d49bdf16a2a003699d9aaa56
fesc7420 <ferdinand.schneider@esolutions.de>
2019-04-02 14:42:47 +0200

    second step: load content script once for each bitbucket tab

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,25 @@
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({color: '#3aa757'}, function () {
-        console.log('The color is green.');
+    let bitbucketBaseUrl: URL = new URL('https://my.local.bitbucket');
+
+    chrome.storage.sync.get('bitbucketHost', data => {
+        console.log('baseUrl is ' + data.bitbucketHost);
+        bitbucketBaseUrl = new URL(data.bitbucketHost);
     });
+    chrome.storage.sync.set({bitbucketHost: 'https://my.local.bitbucket'});
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'my.local.bitbucket'},
+                pageUrl: {hostEquals: bitbucketBaseUrl.hostname},
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
+    });
+
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status === 'complete' && new URL(tab.url).hostname === bitbucketBaseUrl.hostname) {
+            chrome.tabs.executeScript(tabId, {file: 'content.js'});
+        }
     });
 });
