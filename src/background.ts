8928a76e60bf118e2ef9528c326f1174f36989c6
Pfoerd <17569373+Pfoerd@users.noreply.github.com>
2021-06-15 15:55:16 +0200

    first step: calling a GET on bitbucket rest api in content script
    on-behalf-of: @e-solutions-GmbH <info@esolutions.de>

--- src/background.ts
+++ src/background.ts
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
+                pageUrl: {hostEquals: 'my.local.bitbucket'},
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
--- src/popup.ts
+++ src/popup.ts
@@ -10,6 +10,6 @@ changeColor.onclick = function (element) {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
+            {file: 'content.js'});
     });
 };
