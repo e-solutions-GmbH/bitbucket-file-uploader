cc691724b4ae6fad56bf8d3ffcbc3fb72783c924
Pfoerd <17569373+Pfoerd@users.noreply.github.com>
2021-06-15 15:57:20 +0200

    third step: trigger xhr in content script via post message
    on-behalf-of: @e-solutions-GmbH <info@esolutions.de>

--- src/content.ts
+++ src/content.ts
@@ -1,15 +1,24 @@
-const xhr = new XMLHttpRequest();
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'uploadRequest') {
+        const xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-            // Typical action to be performed when the document is ready:
-            console.log('SUCCESS!\n' + xhr.responseText);
-        } else {
-            console.log('ERROR!\n' + xhr.responseText);
-        }
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    // Typical action to be performed when the document is ready:
+                    console.log('SUCCESS!\n' + xhr.responseText);
+                } else {
+                    console.log('ERROR!\n' + xhr.responseText);
+                }
+                sendResponse(xhr.responseText);
+            }
+        };
+        xhr.open('GET', 'https://my.local.bitbucket/rest/api/1.0/projects/my-project/repos/development/browse/how-tos', true);
+        xhr.send();
+        console.log('sending xhr request');
+
+        // must return true to indicate that response will be sent async
+        return true;
     }
-};
-xhr.open('GET', 'https://my.local.bitbucket/rest/api/1.0/projects/my-project/repos/development/browse/how-tos', true);
-xhr.send();
-console.log('sending xhr request');
+});
+
