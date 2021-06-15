4fca251aba3d377eaf52949dc33e46418a2f282d
Pfoerd <17569373+Pfoerd@users.noreply.github.com>
2021-06-15 15:57:34 +0200

    correct configuration of url matchings
    on-behalf-of: @e-solutions-GmbH <info@esolutions.de>

--- src/background.ts
+++ src/background.ts
@@ -1,16 +1,17 @@
 chrome.runtime.onInstalled.addListener(function () {
     let bitbucketBaseUrl: URL = new URL('https://my.local.bitbucket');
 
+    chrome.storage.sync.set({bitbucketHost: 'https://my.local.bitbucket'});
     chrome.storage.sync.get('bitbucketHost', data => {
         console.log('baseUrl is ' + data.bitbucketHost);
         bitbucketBaseUrl = new URL(data.bitbucketHost);
     });
-    chrome.storage.sync.set({bitbucketHost: 'https://my.local.bitbucket'});
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        const regex = bitbucketBaseUrl.hostname.replace(/\./g, '\\.') + '/projects/\\S+/repos/\\S+/browse[^/]*';
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: bitbucketBaseUrl.hostname},
+                pageUrl: {urlMatches: regex},
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
